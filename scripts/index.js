06e85c1601723a89679343e297b027125060b1a5
Elena Ganovich <e.ganovich@yandex.ru>
2020-07-29 10:03:45 +0300

    Изменена структура js, изменены названия некоторых функций

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,96 @@
-//modals
-const editProfileModal = document.querySelector('.modal_type_edit-profile');
-const addCardModal = document.querySelector('.modal_type_add-card');
-const photoModal = document.querySelector('.modal_type_photo')
-const modal = document.querySelector('.modal');
-
-
-//DOM elements
+//Объявляем элементы страницы
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
 const cardsList = document.querySelector('.places');
 
+//Объявляем модальные окна по уникальному селектору
+const editProfileModal = document.querySelector('.modal_type_edit-profile');
+const addCardModal = document.querySelector('.modal_type_add-card');
+const photoModal = document.querySelector('.modal_type_photo')
 
-
-//open-modals-buttons
+//Объявляем кнопки, открывающие модальные окна
 const editProfileModalOpenButton = document.querySelector('.edit-button');
 const addCardModalOpenButton = document.querySelector('.add-button');
 
-
-//close-modals-buttons
+//Объявляем кнопки, закрывающие модальные окна
 const editProfileModalCloseButton = editProfileModal.querySelector('.modal__close-button');
 const addCardModalCloseButton = addCardModal.querySelector('.modal__close-button');
 const photoModalCloseButton = photoModal.querySelector('.modal__close-button');
 
-
-//editProfileModal's elements
-const EditProfileForm = editProfileModal.querySelector('.modal__edit-form');
-const EditProfileFormName = editProfileModal.querySelector('.modal__input_type_name');
-const EditProfileFormAbout = editProfileModal.querySelector('.modal__input_type_about');
-const EditProfileFormSubmitButton = editProfileModal.querySelector('.modal__submit-button')
-
-//addCardModal's elements
+//Объявляем элементы внутри модальных окон
+//editProfileModal
+const editProfileFormName = editProfileModal.querySelector('.modal__input_type_name');
+const editProfileFormAbout = editProfileModal.querySelector('.modal__input_type_about');
+//addCardModal
 const addCardForm = addCardModal.querySelector('.modal__edit-form');
 const addCardFormPlaceName = addCardModal.querySelector('.modal__input_type_place-name');
 const addCardFormPlacePhotoLink = addCardModal.querySelector('.modal__input_type_place-photo-link');
-const addCardFormSubmitButton = addCardModal.querySelector('.modal__submit-button')
-
-//photoModal's elements
+//photoModal
 const modalPhoto = photoModal.querySelector('.modal__photo ');
 const modalPhotoTitle = photoModal.querySelector('.modal__photo-title');
 
-//Card's array
+//Функция открывания и закрывания всех модалок
+function toggleModal(modal) {
+    modal.classList.toggle('modal_is-open');
+}
+
+//Модалка обновления профиля
+editProfileModalOpenButton.addEventListener('click', () => {
+    toggleModal(editProfileModal);
+    updateModalEditProfileForm();
+})
+
+editProfileModalCloseButton.addEventListener('click', () => {
+    toggleModal(editProfileModal);
+})
+
+editProfileModal.addEventListener('submit', submitEditProfileForm);
+
+function updateProfile() {
+    profileName.textContent = editProfileFormName.value;
+    profileAbout.textContent = editProfileFormAbout.value;
+}
+
+function updateModalEditProfileForm() {
+    editProfileFormName.value = profileName.textContent;
+    editProfileFormAbout.value = profileAbout.textContent;
+}
+
+function submitEditProfileForm(e) {
+    e.preventDefault();
+    updateProfile();
+    toggleModal(editProfileModal);
+}
+
+//Модалка добавления новой карточки
+addCardModalOpenButton.addEventListener('click', () => {
+    toggleModal(addCardModal);
+    addCardForm.reset();
+})
+
+addCardModalCloseButton.addEventListener('click', () => {
+    toggleModal(addCardModal);
+    addCardForm.reset();
+})
+
+addCardModal.addEventListener('submit', submitAddCardForm);
+
+function submitAddCardForm(e) {
+    e.preventDefault();
+    renderCard({ name: addCardFormPlaceName.value, link: addCardFormPlacePhotoLink.value });
+    toggleModal(addCardModal);
+}
+
+//Модалка с фотографией
+photoModalCloseButton.addEventListener('click', () => {
+    toggleModal(photoModal);
+})
+
+
+//Генерация карточек
+const templateCard = document.querySelector('.template-card').content.querySelector('.place');
+
+//Массив исходных карточек
 const initialCards = [
     {
         name: 'Архыз',
@@ -68,8 +119,6 @@ const initialCards = [
 ];
 
 
-const templateCard = document.querySelector('.template-card').content.querySelector('.place');
-
 function createCard(data) {
     const cardElement = templateCard.cloneNode(true);
     const cardPhoto = cardElement.querySelector('.place__photo');
@@ -78,6 +127,7 @@ function createCard(data) {
     const cardRemove = cardElement.querySelector('.place__trash');
 
     cardName.textContent = data.name;
+    cardPhoto.alt = data.name + '. Фотография.';
     cardPhoto.src = data.link;
 
     cardRemove.addEventListener('click', (e) => {
@@ -86,12 +136,12 @@ function createCard(data) {
 
     cardLike.addEventListener('click', (e) => {
         e.target.classList.toggle('place__like_active');
-        console.log('111');
     })
 
     cardPhoto.addEventListener('click', (e) => {
         e.target.closest('.place');
         modalPhoto.src = cardPhoto.src;
+        modalPhoto.alt = cardPhoto.alt;
         modalPhotoTitle.textContent = cardName.textContent;
         toggleModal(photoModal);
     })
@@ -107,65 +157,3 @@ initialCards.forEach((data) => {
 function renderCard(data) {
     cardsList.prepend(createCard(data));
 }
-
-
-function profileUpdate() {
-    profileName.textContent = EditProfileFormName.value;
-    profileAbout.textContent = EditProfileFormAbout.value;
-}
-
-function modalEditFormUpdate() {
-    EditProfileFormName.value = profileName.textContent;
-    EditProfileFormAbout.value = profileAbout.textContent;
-}
-
-function toggleModal(modal) {
-    modal.classList.toggle('modal_is-open');
-}
-
-
-editProfileModalOpenButton.addEventListener('click', () => {
-    if (editProfileModal.classList.contains('modal__is-open') === false) {
-        toggleModal(editProfileModal);
-        modalEditFormUpdate();
-    } else {
-        toggleModal(editProfileModal);
-    }
-})
-
-editProfileModalCloseButton.addEventListener('click', () => {
-    toggleModal(editProfileModal);
-})
-
-
-addCardModalOpenButton.addEventListener('click', () => {
-    toggleModal(addCardModal);
-    addCardForm.reset();
-})
-
-addCardModalCloseButton.addEventListener('click', () => {
-    toggleModal(addCardModal);
-    addCardForm.reset();
-})
-
-function editFormSubmitHandler(e) {
-    e.preventDefault();
-    profileUpdate();
-    toggleModal(editProfileModal);
-
-}
-
-editProfileModal.addEventListener('submit', editFormSubmitHandler);
-
-function addCardFormSubmitHandler(e) {
-    e.preventDefault();
-    renderCard({ name: addCardFormPlaceName.value, link: addCardFormPlacePhotoLink.value });
-    toggleModal(addCardModal);
-}
-
-addCardModal.addEventListener('submit', addCardFormSubmitHandler);
-
-photoModalCloseButton.addEventListener('click', () => {
-    toggleModal(photoModal);
-})
-
